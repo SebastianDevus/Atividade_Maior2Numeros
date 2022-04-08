40e40a0ea909676fe59d068aa1106c7203456d05
SebastianDevus <sebastianjogosextra@gmail.com>
2022-04-08 17:42:20 -0300

    Teste de maioridade concluído.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int numero1, numero2;
+
+// Espacamento
+Console.WriteLine();
+
+// Pedindo e armazenando os números
+Console.Write("Digite o primeiro número..: ");
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número...: ");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+// Espacamento
+Console.WriteLine();
+
+// Testando qual dos dois é maior
+if (numero1 > numero2)
+{
+    // Escrevendo o maior
+    Console.WriteLine($"O maior número é {numero1}");
+}
+else if (numero1 < numero2)
+{
+    // Escrevendo o maior
+    Console.WriteLine($"O maior número é {numero2}");
+}
+
+// Espacamento
+Console.WriteLine();
